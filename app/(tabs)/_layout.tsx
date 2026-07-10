e08e2b1d8499694f985d29d5c55c5f2d46854ef0
LucaneTech <contact@lucane.tech>
2026-07-10 23:45:33 +0100

    icon in tab menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,10 @@ import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
-    <NativeTabs tintColor="#ffff">
+    <NativeTabs tintColor="#ffff" backgroundColor="#008080" >
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon />
+        <NativeTabs.Trigger.Icon  sf="gear" md="home"/>
       </NativeTabs.Trigger>
       
       <NativeTabs.Trigger name="setting">
